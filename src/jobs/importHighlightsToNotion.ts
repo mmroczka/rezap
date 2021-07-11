24f75953719efe26fa9b9c3dc34968a1765c615a
Michael Mroczka <mmroczka@salesforce.com>
2021-07-11 11:03:52 -0500

    remove logs

--- src/jobs/importHighlightsToNotion.ts
+++ src/jobs/importHighlightsToNotion.ts
@@ -1,27 +1,23 @@
 import * as dotenv from 'dotenv'
 import { NotionAPI } from '../APIs/notion-api.js'
 import { RescueTimeAPI } from '../APIs/rescue-time-api.js'
-import logger from 'simple-node-logger'
+import { Logger } from '../utils/Logger'
 dotenv.config()
 const jobName: string = '[[Import Highlights Into Notion]]'
 
 const main = async () => {
   const notionAPI = new NotionAPI(jobName)
   const rescueTimeAPI = new RescueTimeAPI(jobName)
-  const job_logger = logger.createSimpleLogger(
-    'logs/importHighlightsToNotion.log'
-  )
-
-  job_logger.log('info', 'Starting rescuetime highlights request')
+  // job_logger.log('info', 'Starting rescuetime highlights request')
   const highlights = await rescueTimeAPI.getRescueTimeHighlights()
   for (const highlight of highlights) {
     const oldHighlight = await notionAPI.findHighlightById(highlight.id)
     if (oldHighlight === undefined) {
-      job_logger.log('info', 'highlight is new!')
+      // job_logger.log('info: highlight is new!')
       const page = notionAPI.convertRescueTimeHighlightToNotionPage(highlight)
-      await notionAPI.createPageInDatabase(page)
+      await notionAPI.addPageInDatabase(page)
     } else {
-      job_logger.log('info', 'highlight already exists!')
+      // job_logger.log('info: highlight already exists!')
     }
   }
 }
