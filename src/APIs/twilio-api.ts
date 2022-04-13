59f76973d79e082e6cbc2038f6be0195f8cfd450
Michael Mroczka <michael.mroczka@gmail.com>
2022-04-13 13:43:50 -0500

    Add Twilio and Email APIs with TextMeWhenIGetAnImportantEmail job

---
+++ src/APIs/twilio-api.ts
@@ -0,0 +1,44 @@
+import * as dotenv from 'dotenv'
+import { Logger } from '../utils/Logger'
+import constants from '../constants.config'
+import { connect, connection, model } from 'mongoose'
+import * as dayjs from 'dayjs'
+dotenv.config()
+
+// dashboard: https://console.twilio.com/
+
+const twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+class TwilioAPIError extends Error {}
+
+export class TwilioAPI {
+  protected twilio 
+  protected logger
+
+  constructor(public jobName: string = 'No Job Name') {
+    this.logger = new Logger(
+      './src/logs/twilio-api.log',
+      '[TWILIO API]',
+      jobName
+    )
+    this.logger.log(`Starting Twilio logger`)
+    this.twilio = twilio
+  }
+
+
+  async sendTextMessage(message: string, to: string) {
+      try{
+        const result = await twilio.messages
+        .create({
+            body: message,
+            to: to,
+            from: '+19124914714'
+        })
+        .then((message: any) => console.log(message.sid));
+        return result
+      } catch(e) {
+
+      }
+  }
+
+}
